68cbe2d26fd626eb5dd76ae33374717c339e8439
Terry Lewis <terrylewis@outlook.com>
2014-02-02 14:23:31 -0600

    Added YapDatabase pod and updated .gitignore.

---
+++ Pods/Pods-environment.h
@@ -0,0 +1,35 @@
+
+// To check if a library is compiled with CocoaPods you
+// can use the `COCOAPODS` macro definition which is
+// defined in the xcconfigs so it is available in
+// headers also when they are imported in the client
+// project.
+
+
+// CocoaLumberjack
+#define COCOAPODS_POD_AVAILABLE_CocoaLumberjack
+// This library does not follow semantic-versioning,
+// so we were not able to define version macros.
+// Please contact the author.
+// Version: 1.6.5.1.
+
+// CocoaLumberjack/Core
+#define COCOAPODS_POD_AVAILABLE_CocoaLumberjack_Core
+// This library does not follow semantic-versioning,
+// so we were not able to define version macros.
+// Please contact the author.
+// Version: 1.6.5.1.
+
+// CocoaLumberjack/Extensions
+#define COCOAPODS_POD_AVAILABLE_CocoaLumberjack_Extensions
+// This library does not follow semantic-versioning,
+// so we were not able to define version macros.
+// Please contact the author.
+// Version: 1.6.5.1.
+
+// YapDatabase
+#define COCOAPODS_POD_AVAILABLE_YapDatabase
+#define COCOAPODS_VERSION_MAJOR_YapDatabase 2
+#define COCOAPODS_VERSION_MINOR_YapDatabase 3
+#define COCOAPODS_VERSION_PATCH_YapDatabase 0
+
