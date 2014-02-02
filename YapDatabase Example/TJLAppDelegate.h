195e9c400c588a7965cb5741bd06aef7add36b60
Terry Lewis <terrylewis@outlook.com>
2014-02-02 14:17:22 -0600

    Initial Commit

---
+++ YapDatabase Example/TJLAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  TJLAppDelegate.h
+//  YapDatabase Example
+//
+//  Created by Terry Lewis II on 2/2/14.
+//  Copyright (c) 2014 Blue Plover Productions LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TJLAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ YapDatabase Example/TJLViewController.h
@@ -0,0 +1,13 @@
+//
+//  TJLViewController.h
+//  YapDatabase Example
+//
+//  Created by Terry Lewis II on 2/2/14.
+//  Copyright (c) 2014 Blue Plover Productions LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TJLViewController : UIViewController
+
+@end
